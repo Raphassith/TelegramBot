0a7022fe564eaa72c0de1de90006a2f523f6df5b
รภัสสิทธิ์ กำจรอัครหิรัญ <34786940+Raphassith@users.noreply.github.com>
2025-01-29 12:02:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const messagePayload = {
+  chat_id: "YOUR_CHAT_ID",
+  text: "ข้อความพร้อมฟอร์แมต *ตัวหนา* และ _ตัวเอียง_",
+  parse_mode: "Markdown",
+};
+
 const sendTelegramData = async (endpoint, payload) => {
   const token = "YOUR_BOT_TOKEN";
   const url = `https://api.telegram.org/bot${token}${endpoint}`;
